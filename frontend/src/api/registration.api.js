6a547a556460a60f4ff4123be88a433ec514f03b
xyvintech <admin@xyvin.com>
2025-09-15 16:18:24 +0530

    t

--- frontend/src/api/registration.api.js
+++ frontend/src/api/registration.api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/registrations";
+const API_URL = import.meta.env.VITE_API_URL||"http://localhost:5454/api/registrations";
 
 export const createRegistration = async (registrationData) => {
-  const response = await axios.post(API_URL, registrationData);
+  const response = await axios.post(`${API_URL}/registrations`, registrationData);
   return response.data;
 };
 
 export const getRegistrations = async () => {
-  const response = await axios.get(`${API_URL}?page=1&limit=10`);
+  const response = await axios.get(`${API_URL}/registrations?page=1&limit=10`);
   return response.data;
 };

1ca29300a721c224508194fd7346c006037d3835
xyvintech <admin@xyvin.com>
2025-09-15 18:06:01 +0530

    asd

--- frontend/src/api/registration.api.js
+++ frontend/src/api/registration.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL||"http://localhost:5454/api/registrations";
+const API_URL = import.meta.env.VITE_API_URL||"https://api-onavesham2.unma.in/api";
 
 export const createRegistration = async (registrationData) => {
   const response = await axios.post(`${API_URL}/registrations`, registrationData);

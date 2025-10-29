c6edbf901712aa43eb0c890ee4c450b3a63599fa
xyvintech <admin@xyvin.com>
2025-10-29 18:31:26 +0530

    tesat

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-// Compression middleware
+// Compression middlewares
 app.use(compression());
 
 // Logging middleware
